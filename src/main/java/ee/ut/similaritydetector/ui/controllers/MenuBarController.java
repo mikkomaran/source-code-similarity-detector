52fb5f433b1568302a3df592533668d5e279d43d
Mikko Maran <mixxo.maran@gmail.com>
2021-09-17 12:21:14 +0300

    Version changed to v2.2

--- src/main/java/ee/ut/similaritydetector/ui/controllers/MenuBarController.java
+++ src/main/java/ee/ut/similaritydetector/ui/controllers/MenuBarController.java
@@ -184,7 +184,7 @@ public class MenuBarController {
     @FXML
     private void showAboutInfo() {
         ResourceBundle langBundle = ResourceBundle.getBundle("ee.ut.similaritydetector.language.menu_bar", UserPreferences.getInstance().getLocale());
-        showAlert( langBundle.getString("app_name") + " v2.1",
+        showAlert( langBundle.getString("app_name") + " v2.2",
                 "Mikko Maran\n" + "2021",
                 Alert.AlertType.INFORMATION);
     }
